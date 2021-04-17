5819d2453148e70ddc167f54d4519ec50956420e
kcode <31813475+kdr-s@users.noreply.github.com>
2021-04-17 18:07:02 +0900

    Redundant code removal

--- clipboard-img-to-Y.py
+++ clipboard-img-to-Y.py
@@ -1,15 +1,10 @@
 from PIL import ImageGrab, ImageChops, Image
-from datetime import datetime
 from time import sleep
-from os import makedirs
-from os.path import expanduser
 from ctypes import windll
 import numpy as np
 
 last_im = Image.new("RGB", (512, 512), (128, 128, 128))
-folder = expanduser("~") + "/Pictures/Screenshots/"
 Ctrl = 0x11
-makedirs(folder, exist_ok=True)
 
 def isPressed(key):
     return(bool(windll.user32.GetAsyncKeyState(key)&0x8000))
@@ -20,9 +15,6 @@ while True:
         im = im.convert('RGB')
         # ２つの画像の同一かどうか
         if ImageChops.difference(im, last_im).getbbox() != None:
-            # クリップボードにあるRGB画像を保存したい場合は、以下のコメントを解除します
-            # im.save(folder + datetime.now().strftime("%Y%m%d-%H%M%S") + ".png")
-            # 保存機能を使う場合、以下もコメントを解除します。同じ画像を保存することを防ぎます。
             last_im = im
             if isPressed(Ctrl):
                 # lumi_im = im.convert("L")
